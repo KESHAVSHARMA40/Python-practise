3a41f30ebf1c4b7ea03e7462ebbcf9671146d246
Keshav Sharma <k.sharmayyz@gmail.com>
2025-11-08 02:42:29 -0500

    Initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,14 @@
+Name = "Keshav"
+Age = 23
+Occupation = "Security Guard"
+print(Name)
+print(Age)
+print(Occupation)
+print("My Name is : ",Name)
+print("My Age is : ",Age)
+print("I work as a",Occupation)
+print(10+20+30,Name)
+print(type(Name))
+print(type(Age))
+print(type(Occupation))
+
---
+++ Operations.py
@@ -0,0 +1,26 @@
+"""
+This is the multi-line comment
+"""
+
+#This is the code for the operations that can be used in the python.
+
+# Arithmetic Operators
+A = 5
+B = 4 
+C = 10
+D = 44
+print(A+B)
+print(10+C+D)
+print(D*C)
+print(C/A)
+
+# Relational Operators
+
+print(A>B) #True
+print(A<B) #False
+print(A==B) #False
+print(A!=B) #True
+print(A>=B) #True
+print(A<=B) #False
+
+#
