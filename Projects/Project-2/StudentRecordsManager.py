66f5c532cb10e594ddeae9efe64fa042b86297ff
Keshav Sharma <k.sharmayyz@gmail.com>
2025-11-29 01:02:56 -0500

    Update the Project-2 with new features

--- Projects/Project-2/StudentRecordsManager.py
+++ Projects/Project-2/StudentRecordsManager.py
@@ -8,14 +8,14 @@ while True:
     print("\n1. Add Student")
     print("2. View All Students")
     print("3. Search Student")
-    print("4. Update Marks")
+    print("4. Update information")
     print("5. Delete Student")
-    print("6. Analytics")
-    print("7. Exit")
+    print("6. Exit")
+    
 
-    choice = input("Enter choice: ")
+    choice = int(input("Enter choice: "))
 
-    if choice == "1":
+    if choice == 1:
                 Roll = int(input("Enter your Roll number: "))
                 name = input("Please Enter the name of the student: ")
                 age = int(input("Please enter the age of the student: "))
@@ -27,32 +27,46 @@ while True:
                 }
                 print("Thank you for filling the details")
 
-    elif choice == "2":
+    elif choice == 2:
 
             for roll, details in students.items():
                   print(f"\nRoll: {roll}")
-            for keys, values in details.items() :
-                  print(f"{keys.capitalize}: {values}")
+                  for keys, values in details.items() :
+                       print(f"{keys}: {values}")
 
-    elif choice == "3":
+    elif choice == 3:
             
-              search = input("Please enter the name of the student you want to search for: ")
-              for s,v in students,students.values():
-                    if students.get("Name") == search :
-                          print(s,":",v)
+              roll = int(input("Enter the roll number:"))
+              if roll in students:
+                    print(students[Roll])
+              else:
+                    print("Student not found")
               
               
+    elif choice == 4:
               
-                    
-        
-              
-    elif choice == "4":
-        pass  # TODO: update
-    elif choice == "5":
-        pass  # TODO: delete
-    elif choice == "6":
-        pass  # TODO: analytics
-    elif choice == "7":
+                roll = int(input("Enter roll number to update: "))
+                if roll in students:
+                    students[roll]["name"] = input("New name: ")
+                    students[roll]["age"] = int(input("New age: "))
+                    students[roll]["city"] = input("New city: ")
+                    print("Updated!")
+                else:
+                    print("Not found.")
+
+       
+    elif choice == 5:
+
+                roll = int(input("Enter roll number to delete: "))
+                if roll in students:
+                        del students[roll]
+                        print("Deleted!")
+                else:
+                        print("Student not found.")
+
+
+    elif choice == 6:
         break
     else :
+        print("Invalid choice, TRY-AGAIN")
         break
