a26c227a3f89c5c66f3fb76290ec9cf55f1d84ef
Keshav Sharma <k.sharmayyz@gmail.com>
2025-12-01 08:30:58 -0500

    Started Module updates

---
+++ 6.Modules/1.Modules.py
@@ -0,0 +1,21 @@
+#  Modules are the python files that I can reuse at the times when needed in the main program.
+
+#  eg: if I create a math_tools.py in which I have create functions of operators used in calculations.
+
+#  I can use that python file in some other code let's say main.py
+
+#  the python file can be used fully by just importing using the code below: 
+#  import math_tools
+
+#  or a single function can be used by importing a single functions: 
+# from math_tools import add
+
+
+#  there are few built-in modules mentioned below:
+
+# 1. os --> Work with files, folders, paths
+# 2. random --> Generate random IDs, OTPs, tokens
+# 3. datetime --> Time, Dates, Timestamps
+# 4. json --> APIs literally communicate in json
+# 5. math --> for math operations
+# 6. sys --> things like system paths, args, execution control.
