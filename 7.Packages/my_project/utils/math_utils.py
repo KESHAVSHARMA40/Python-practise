2df6598aa3b14bec5ad0128026cadd029e3d1b11
Keshav Sharma <k.sharmayyz@gmail.com>
2025-12-04 17:15:57 -0500

    practise project packages

---
+++ 7.Packages/my_project/utils/math_utils.py
@@ -0,0 +1,13 @@
+import __init__
+
+def add(a,b):
+   return a+b
+
+def sub(a,b):
+   return a-b
+
+def multi(a,b):
+   return a*b
+
+def divide(a,b):
+   return a/b
---
+++ 7.Packages/my_project/utils/string_utils.py
@@ -0,0 +1,11 @@
+import __init__
+
+def capitalize_text(text):
+
+  
+   return text.capitalize()
+
+def reverse_string(text):
+   
+   
+   return text[::-1]
