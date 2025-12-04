886fcf8d799b803ecbf7e055b03ae0da59268a08
Keshav Sharma <k.sharmayyz@gmail.com>
2025-12-04 17:11:14 -0500

    packages

---
+++ 7.Packages/1.Packages.py
@@ -0,0 +1,5 @@
+import __init__
+
+#  A package is just a folder that contains python modules.
+#  A PACKAGE folder should always contains a __init__.py file to ensure that it is not just a folder but a package with the python files.
+
